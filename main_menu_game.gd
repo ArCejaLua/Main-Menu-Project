2727f1cf8c88317921739deb6df8b20c983b03b2
ArCeja-Lua <161160515+ArCejaLua@users.noreply.github.com>
2026-01-17 20:14:30 -0800

    Version 2
    i added the button functionality by creating a script and making sure the game runs properly

---
+++ main_menu_game.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://options.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
